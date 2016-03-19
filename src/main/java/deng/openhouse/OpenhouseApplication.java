188aac31987edad7884eff4133fb44beeac63c11
Zemian Deng <saltnlight5@gmail.com>
2016-03-19 11:37:11 -0400

    Added spring boot web project.

---
+++ src/main/java/deng/openhouse/OpenhouseApplication.java
@@ -0,0 +1,12 @@
+package deng.openhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenhouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenhouseApplication.class, args);
+	}
+}
---
+++ src/main/java/deng/openhouse/ServletInitializer.java
@@ -0,0 +1,13 @@
+package deng.openhouse;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OpenhouseApplication.class);
+	}
+
+}
